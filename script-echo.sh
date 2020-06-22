cecec4e7d5cd8bf22e03a748fab74b5bc6747f81
KleilsonSantos <kdsdesign1@gmail.com>
2020-06-22 17:21:07 -0300

    Creating -> script-echo.sh

---
+++ script-echo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#------------------------------------------------------------------------
+# Name Script:	script-echo.sh
+# Information:	https://github.com/KleilsonSantos/Shell-Script
+# Creation in:	June 2, 2020
+# Author:	Kleilson Santos <canalnovoti@gmail.com>
+# Description:	Imprimindo textos e a data do sistema
+# License:	GPL v3
+# Version:	0.0.1
+#------------------------------------------------------------------------
+
+# -> Inicio do script
+# Comando echo, imprime o conteudo do que esta entre as aspas -> ""
+echo "===================================="
+echo "Imprimindo data atual do sistema" 
+
+# Comando date, imprime a data atual do sistema
+date
+echo "===================================="
+# -> Fim do script
